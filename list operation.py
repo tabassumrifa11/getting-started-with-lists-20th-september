4ec1d797c6988a4cef9fdfe5d205fc25aa17ee4f
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-09-20 18:05:32 +0600

    first commit

---
+++ list operation.py
@@ -0,0 +1,19 @@
+li = ["Junaira", "Tabassum", "Kashfia"]
+print(li)
+
+print(  len(li)  )
+
+
+print(  li[0]  )
+print(  li[1]  )
+print(  li[2]  )
+
+li.append("Nahian")
+li.append("giga")
+print(  len(li)  )
+print(  li[3]  )
+print(  li[4]  )
+
+alist = [100, 200, 300, 400, 500]
+
+print(   alist[::-1]  )
